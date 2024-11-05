e140b3622ac967dd55c5aac6fbd4c87abc912c06
jacobhicks13 <166049337+jacobhicks13@users.noreply.github.com>
2024-11-05 14:26:33 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -19,6 +19,52 @@ DB_CONFIG = {
     'port': "3306"  
 }
 
+#password reset
+@app.route('/passwordReset', methods=['POST', 'GET'])
+def passwordReset():
+    
+    if request.method == 'POST':
+        email = request.form.get('username')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_pw')
+
+        if password != confirm_password:
+            flash("Passwords do not match")  # Flash the message
+            return redirect(url_for('passwordReset'))
+
+        connection = mysql.connector.connect(**DB_CONFIG)
+        cursor = connection.cursor()
+
+        try:
+            cursor.execute('SELECT * FROM cs_admin WHERE email = %s', (email,))
+            result = cursor.fetchone()
+
+            if  result is None:
+                flash("The email entered does not have an account.")
+                return(redirect(url_for('passwordReset')))
+            
+            db_email, db_salt, db_hashed_password = result
+            password_bytes = password.encode('utf-8')
+            salt_bytes = bytes.fromhex(db_salt)
+            hashed_password = hashlib.pbkdf2_hmac('sha256', password_bytes, salt_bytes, 100000, dklen=32)
+
+            #changes password for email fetched
+            cursor.execute('UPDATE cs_admin SET pw_key = %s WHERE email = %s', (hashed_password.hex(), email))
+            connection.commit()
+            print("", email)
+            print("", password)
+            if cursor.rowcount == 0:
+                print("It didn't work")
+            else:
+                print("it did work", cursor.rowcount)
+            return redirect(url_for('login'))
+        
+        finally:
+            cursor.close()
+            connection.close()
+    
+    return render_template('passwordReset.html')
+
 @app.route('/', methods=['POST', 'GET'])
 def login():
 
@@ -32,7 +78,7 @@ def login():
 
         try:
             # Query the database to check if the user exists
-            cursor.execute("SELECT email, salt, pw_key FROM user_info WHERE email = %s", (email,))
+            cursor.execute("SELECT email, salt, pw_key FROM cs_admin WHERE email = %s", (email,))
             result = cursor.fetchone()
 
             if result is None:
@@ -87,7 +133,7 @@ def create_account():
 
         try:
             # Check if the username already exists
-            cursor.execute("SELECT * FROM user_info WHERE email = %s", (email,))
+            cursor.execute("SELECT * FROM cs_admin WHERE email = %s", (email,))
             count = cursor.fetchone()
 
             if count is not None:
@@ -105,7 +151,7 @@ def create_account():
             hashed_password = hashlib.pbkdf2_hmac('sha256', password_bytes, salt, iterations, dklen=key_length)
 
             # Store user in database
-            cursor.execute("INSERT INTO user_info (email, salt, pw_key) VALUES (%s, %s, %s)", (email, salt.hex(), hashed_password.hex()))
+            cursor.execute("INSERT INTO cs_admin (email, salt, pw_key) VALUES (%s, %s, %s)", (email, salt.hex(), hashed_password.hex()))
             connection.commit()
             return redirect(url_for('login'))
 
