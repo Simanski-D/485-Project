16017582ba21c4be9222fec310e4417fc57d9333
Gretch <nigburgj@gmail.com>
2024-11-07 14:06:09 -0600

    hardcode data points and adjusting the randomizer

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Define an array of points to project on the map
 const points = [
     { coords: [44.8113, -91.498], label: 'Point 1' },
+    { coords: [-79.69945802, 53.58869902], label: 'Point 2' },
+    { coords: [22.52146577, 78.13385823], label: 'Point 1' },
+    { coords: [-24.22448156, 150.9585045], label: 'Point 1' },
+    { coords: [9.510528056,	-75.8855839], label: 'Point 1' },
+    { coords: [65.96210701,	42.84632654], label: 'Point 1' },
 ];
 
 // Loop through the points and add them to the map
