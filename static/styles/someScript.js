115665320760a43a3981bb0d72c04735b01e3124
Simanski-D <152226168+Simanski-D@users.noreply.github.com>
2024-12-04 11:42:57 -0600

    add file from main

---
+++ static/styles/someScript.js
@@ -0,0 +1,13 @@
+// Expand Information on Dashboard
+var div = document.getElementById('hidden');
+var display = 0;
+
+function hideShow() {
+    if(display === 1) {
+        div.style.display = 'block';
+        display = 0;
+    } else {
+        div.style.display = 'none';
+        display = 1;
+    }
+}
